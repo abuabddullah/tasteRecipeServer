840769b7283414c436f77c83d9ec5ff7b86d64c6
asifaowadud <asifaowadud@gmail.com>
2024-06-01 01:18:48 +0600

    users api creating

--- index.js
+++ index.js
@@ -76,6 +76,54 @@ async function run() {
       res.send(result);
     });
 
+    // create usersDb here
+    const usersDb = client.db("usersDb");
+    const usersCollection = usersDb.collection("usersCollection");
+
+    // basic POST req for creating a user if not exits
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+      const isExistingUser = await usersCollection.findOne(query);
+      if (isExistingUser?.email) {
+        return res.send({
+          status: "success",
+          message: "Login Successfull.User already exists!",
+        });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // GET req for getting UserInfo in DB by id for "editProfile" page
+    app.get("/users/get/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
+
+    // GET req for getting UserInfo in DB by email for "Profile" page
+    app.get("/users/:email", async (req, res) => {
+      const { email } = req.params;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
+
+    // PATCH req for updating UserInfo in DB
+    app.patch("/users/:email", async (req, res) => {
+      const { email } = req.params;
+      const updatedUser = req.body;
+      const query = { email }; // { email: email }
+      const updateDoc = {
+        $set: updatedUser,
+      };
+      const options = { upsert: true }; // if the document does not exist, insert it
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
     console.log("Successfully connected to MongoDB!");
   } finally {
   }
